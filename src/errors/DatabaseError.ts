b6bd8147b728892709bfb17c0571874a20028688
Farbod Shams <farbodshams.2000@gmail.com>
2021-09-28 00:00:05 +0330

    added documentation

--- src/errors/DatabaseError.ts
+++
@@ -1,9 +0,0 @@
-import NGNSilentError from "./NGNSilentError";
-
-class DatabaseError extends NGNSilentError{
-    constructor(message: string) {
-        super(500, message);
-    }
-}
-
-export default DatabaseError;
