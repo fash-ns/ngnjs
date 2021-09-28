0042490f63c08a56b4399f48e0c5082b1eb1ae36
Farbod Shams <farbodshams.2000@gmail.com>
2021-09-28 14:00:47 +0330

    added validation document

--- src/middlewares/withValidator.ts
+++ src/middlewares/withValidator.ts
@@ -4,13 +4,12 @@ import {NextHandler} from "../types/NextHandler";
 import {Middleware} from "../types/Middleware";
 import Keyable from "../types/Keyable";
 import Rule from "../services/validator/rules/Rule";
-import Validator from "../services/validator/Validator";
+import validate from "../services/validator/validate";
 import {ValidationResult} from "../types/ValidationResult";
 import ValidationError from "../errors/ValidationError";
 
 const validator = (req: NGNApiRequest) => async (validationObject: Keyable<Array<Rule>>) => {
-    const validator: Validator = new Validator();
-    const validationRes: ValidationResult = await validator.validate(validationObject, req.all);
+    const validationRes: ValidationResult = await validate(validationObject, req.all);
     if(!validationRes.result)
         throw new ValidationError("Given data is not valid", {errors: validationRes.errors});
 }
---
+++ src/services/validator/validate.ts
@@ -0,0 +1,26 @@
+import Keyable from "../../types/Keyable";
+import Rule from "./rules/Rule";
+import {ValidationResult} from "../../types/ValidationResult";
+
+const validate = async (rules: Keyable<Array<Rule>>, data: Keyable): Promise<ValidationResult> => {
+    let errors: Keyable = {};
+    for (let key in rules) {
+        if(rules.hasOwnProperty(key)) {
+            let value = data[key] ?? null;
+            let keyRules: Array<Rule> = rules[key];
+            for (let i = 0; i < keyRules.length; i++) {
+                let rule: Rule = keyRules[i];
+                if (!await rule.evaluate(value)) {
+                    errors[key] = rule.getError(key);
+                    break;
+                }
+            }
+        }
+    }
+    return {
+        result: Object.keys(errors).length == 0,
+        errors: errors,
+    };
+}
+
+export default validate;
